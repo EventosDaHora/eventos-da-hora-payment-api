1d11ad55aa65df8b41386e7a24fc7f0cfdaf5643
Matheus <matheus.etb@gmail.com>
2020-09-14 23:31:49 -0300

    Estrutura inicial do ms de pagamento

---
+++ src/test/java/com/eventosdahora/payment/ms/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.eventosdahora.payment.ms;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
