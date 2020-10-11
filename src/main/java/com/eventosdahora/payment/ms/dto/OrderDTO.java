886abba31ca75d7d8d2204499d68d813a1cb3eaf
italoeduardo96@hotmail.com <italoeduardo96@hotmail.com>
2020-10-11 15:57:44 -0300

    Ajustar LocalDateTime

--- src/main/java/com/eventosdahora/payment/ms/dto/OrderDTO.java
+++ src/main/java/com/eventosdahora/payment/ms/dto/OrderDTO.java
@@ -7,6 +7,7 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -20,7 +21,7 @@ public class OrderDTO {
 	
 	private Long orderId;
 	
-	private LocalDate createdDate;
+	private LocalDateTime createdDate;
 	
 	private OrderState orderState;
 	
